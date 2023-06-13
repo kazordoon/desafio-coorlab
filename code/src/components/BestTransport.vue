7b7e5796de980d57488f0815989c9956d3ca7b7e
Felipe Barros <felipetech@tuta.io>
2023-06-13 09:25:17 -0300

    feat: Get freight destinations

--- code/src/components/BestTransport.vue
+++ code/src/components/BestTransport.vue
@@ -5,8 +5,24 @@
         <b>{{ appName }}</b>
       </b-navbar-brand>
     </b-navbar>
-  </div>
 
+    <div>
+      <form>
+        <div>
+          <label for="destination">Destino</label>
+          <select name="destination" id="destination">
+            <option value="" selected disabled>Selecione o destino</option>
+            <option v-for="destination in destinations" :key="destination" :value="destination">{{ destination }}</option>
+          </select>
+        </div>
+        <div>
+          <label for="weight">Peso</label>
+          <input type="text" />
+        </div>
+        <button>Analisar</button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,6 +30,7 @@ import {
   BNavbar,
   BNavbarBrand,
 } from 'bootstrap-vue'
+import Request from '../helpers/Request.js'
 
 export default {
   components: {
@@ -22,15 +39,28 @@ export default {
   },
   data() {
     const appName = ''
+    const destinations = []
 
     return {
       appName,
+      destinations
     }
   },
-  created() {
+  async created() {
     // Implemente aqui o GET dos dados da API REST
     // para que isso ocorra na inicialização da pagina
     this.appName = 'Melhor Frete'
+
+    const API_URL = 'http://localhost:3000/transport'
+    const freightTypes = await Request.GET(API_URL)
+
+    freightTypes.forEach(freight => {
+      if (!(freight.city in this.destinations))
+        this.destinations.push(freight.city)
+
+      this.destinations = [...(new Set(this.destinations))]
+    })
+
   },
   methods: {
     // Implemente aqui os metodos utilizados na pagina
