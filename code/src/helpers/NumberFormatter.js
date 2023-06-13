2f663b656c6523013873e6e7c2430190cbe74d6b
Felipe Barros <felipetech@tuta.io>
2023-06-13 19:58:34 -0300

    feat(helpers/NumberFormatter): Add method to convert string hours to number

--- code/src/helpers/NumberFormatter.js
+++ code/src/helpers/NumberFormatter.js
@@ -3,6 +3,13 @@ export default class NumberFormatter {
    * @param {string} currency 
    */
   static currencyToNumber(currency) {
-    return Number(currency.replace(/R\$\s/, ''))
+    return Number(currency.replace('R$ ', ''))
+  }
+
+  /**
+   * @param {string} hours 
+   */
+  static hoursToNumber(hours) {
+    return Number(hours.replace('h', ''))
   }
 }
